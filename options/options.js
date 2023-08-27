150fab4931c6a25f20ecaac26fda8b4e123acdcd
Crowdbotics <engineering@crowdbotics.com>
2023-08-27 23:11:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "noisy-river-dev-111990",
+  url: "https://noisy-river-dev-111990.botics.co",
+  api: "https://noisy-river-dev-111990.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
